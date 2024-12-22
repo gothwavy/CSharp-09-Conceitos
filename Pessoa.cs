3ca836a41a1679c63b87cc072f37df6b444d6165
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:03:38 -0300

    Finalizado✅

---
+++ Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_This
+{
+    internal class Pessoa
+    {
+        // Atributo
+        private string nome = "Tatiana";
+
+        // Construtor
+        public Pessoa(string nome)
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(this.nome);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _09_This
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa Ryan = new Pessoa("Ryan");
+            // O this especifica dentro de um metodo que o item usado sera um atributo e nao um parametro.
+        }
+    }
+}
